6c2c155fd14a460d7d3efbf99590dfdb1ed641a1
nishat2612 <93609061+nishat2612@users.noreply.github.com>
2022-09-21 15:19:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>| Online Quiz Application |</title>
+        <link rel="stylesheet" type="text/css" href="css/index.css" />
+        <link rel="shortcut icon" type="image/png" href="image/123.jpg" />
+        <style type="text/css">
+            body {
+                width: 100%;
+                background: url(image/123.jpg) ;
+                background-position: center center;
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: cover;
+            }
+        </style>
+    </head>
+    <body>
+        <center>
+            <div class="intro">
+                <h1> Online Quiz Application </h1>
+                <a href="login.php" class="btn"> login </a> &emsp;
+                <a href="register.php" class="btn"> register </a>
+                <h2> Good &nbsp;Luck. </h2>
+            </div>
+        </center>
+    </body>
+</html>
+
+
